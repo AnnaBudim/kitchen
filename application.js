3a2451bb4d1b38c3e5452f888771623d6600a9e1
Anna Budim <20anna08@ukr.net>
2017-08-02 11:31:52 +0000

    add smth

---
+++ application.js
@@ -0,0 +1,26 @@
+$(function() {
+    $('form input[type=submit]').click(sendForm);
+})
+
+
+function sendForm(e) {
+    e.preventDefault();
+    $.ajax({
+        url: "https://formspree.io/20Anna08@ukr.net",
+        method: "POST",
+        data: {
+            firstname: $('#firstname').val(),
+            secondname: $('#secondname').val(),
+            email: $('#email').val(),
+            comment: $('#comment').val()
+        },
+        dataType: "json"
+    })
+     .done(function() {
+         $('form')[0].reset();
+         $('#form-result').html('<p>Thank you!</p>');
+     })
+     .fail(function(xhr, status, error){
+         $('#form-result').html(`<p>Sorry, there is an error: ${error}</p>`);
+     });
+}
